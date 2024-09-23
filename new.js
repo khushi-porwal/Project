ee05e04f68491025758f4d50593855e0eaee908c
khushi-porwal <khushiporwal98@gmail.com>
2024-09-23 13:10:02 +0530

    ASB Design

---
+++ new.js
@@ -0,0 +1,50 @@
+// x=false;
+// console.log(x);
+// let name="tony";
+// console.log(name);
+// let a=BigInt("123");
+// // console.log(a);
+// const stu ={
+//     age: 23,
+//     fullname: "khushi",
+//     cgpa: 5.4,
+// }
+// let b=3;
+// let c=4;
+// console.log("b+c",b+c);
+// console.log("a++=:",b++);
+// console.log(b)
+// console.log(--c)
+// console.log(a==b)
+// console.log(a!=b);
+// y=3;
+// z=5;
+// cond1=y<z;
+// cond2=y==3;
+// console.log(cond1||cond2);
+// let color=12;
+// if(color>=19) {
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you cannot vote");
+// }
+// let n=4;
+// n>3?console.log("true"):console.log("true");
+// console.log(n)
+// function calculator() {
+//     let a = document.getElementById("search").value;
+//     // console.log(a);
+//     if(a>=5) {
+//         console.log("a is greater");
+//     }
+//     else{
+//         console.log("A is less than 5");
+//     }
+// }
+console.log("simplefunctions");
+function addthreenumbers(a, b, c) {
+    return a+b+c;
+}
+let c = addthreenumbers(1, 2, 56);
+console.log(c);
